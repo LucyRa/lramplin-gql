7499f3da2a6a17e6c5cb60d3247ea8a4a84c53e3
LucyRa <lucyramplin@gmail.com>
2022-07-19 21:51:19 +0100

    Adding log out button to sidebar

--- portal/resources/views/dashboard.blade.php
+++ portal/resources/views/dashboard.blade.php
@@ -1,12 +1,7 @@
 <x-app-layout>
   <header>
-    <form action="{{ route('logout') }}" method="POST">
-      @csrf
-      <button>Logout</button>
-    </form>
+    
   </header>
   
-  <div>
-    This is the logged in view
-  </div>
+  
 </x-app-layout>
--- portal/resources/views/livewire/sidebar.blade.php
+++ portal/resources/views/livewire/sidebar.blade.php
@@ -1,3 +1,18 @@
-<nav>
-    
+<nav class="sidebar" role="main">
+  <div class="controls">
+
+  </div>
+
+  <div class="nav-items-container">
+    <div class="nav-items">
+
+    </div>
+
+    <form action="{{ route('logout') }}" method="POST">
+      @csrf
+      <button>
+        <livewire:logout-icon/>
+      </button>
+    </form>
+  </div>
 </nav>
