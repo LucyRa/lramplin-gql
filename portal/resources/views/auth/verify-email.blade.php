249aba8e7afc693dc05800a9cd8a39d557dd1133
LucyRa <lucyramplin@gmail.com>
2022-07-20 13:26:16 +0100

    Basic styling of verify page

--- portal/resources/views/auth/verify-email.blade.php
+++ portal/resources/views/auth/verify-email.blade.php
@@ -4,46 +4,48 @@
       Verify Email
     </h1>
 
-    <p>
-      Thanks for signing up! Before getting started, could you verify your email address by clicking on the link we just sent you? If you didn't recieve the email, we will gladly send you another.
-    </p>
-    
-    @if ($errors->any())
-      <div>
-        <p>Something went wrong!</p>
-        
-        <ul>
-          @foreach($errors->all() as $error)
-          <li>
-            {{ $error }}
-          </li>
-          @endforeach
-        </ul>
-      </div>
-    @endif
-
-    @if (session('status') == 'verification-link-send')
+    <div class="form-block">
       <p>
-        A new verification link has been sent to the email address you provided during registration.
+        Thanks for signing up! Before getting started, could you verify your email address by clicking on the link we just sent you? If you didn't recieve the email, we will gladly send you another.
       </p>
-    @endif
+      
+      @if ($errors->any())
+        <div>
+          <p>Something went wrong!</p>
+          
+          <ul>
+            @foreach($errors->all() as $error)
+            <li>
+              {{ $error }}
+            </li>
+            @endforeach
+          </ul>
+        </div>
+      @endif
 
-    <div>
-      <form action="{{ route('verification.send') }}" method="POST" class="button-form">
-        @csrf
+      @if (session('status') == 'verification-link-send')
+        <p>
+          A new verification link has been sent to the email address you provided during registration.
+        </p>
+      @endif
 
-        <button class="btn">
-          Resend Verification Email
-        </button>
-      </form>
+      <div class="form-container-btns">
+        <form action="{{ route('verification.send') }}" method="POST" class="button-form">
+          @csrf
 
-      <form action="{{ route('logout') }}" method="POST" class="button-form">
-        @csrf
+          <button class="btn">
+            Resend Verification Email
+          </button>
+        </form>
 
-        <button class="btn">
-          logout
-        </button>
-      </form>
+        <form action="{{ route('logout') }}" method="POST" class="button-form">
+          @csrf
+
+          <button class="btn-outline">
+            logout
+          </button>
+        </form>
+      </div>
     </div>
   </div>
 </x-guest-layout>
